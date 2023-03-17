194a3c41b34cbd3c2372b26da7062ed965c4bda6
Aadil29 <muhammad.ghani.3@city.ac.uk>
2023-03-17 00:02:37 +0000

    Made a start on the Office manager Blanks GUI implementation

---
+++ src/Manager/OfficeManagerBlanks.java
@@ -0,0 +1,17 @@
+package Manager;
+
+import javax.swing.*;
+
+public class OfficeManagerBlanks {
+    private JPanel panel1;
+    private JButton logOutButton;
+    private JButton homeButton;
+    private JButton stockButton;
+    private JButton blanksButton;
+    private JButton domesticSalesReportButton;
+    private JButton interlineSalesReportButton;
+    private JButton discountPlanButton;
+    private JButton ticketStockTurnOverButton;
+    private JTable table1;
+    private JButton submitBlankUsageReportButton;
+}
